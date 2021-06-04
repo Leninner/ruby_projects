d81a4a973d8633f9c4ce8cd6379e6b6c11afffcf
Leninner <Mazabandalenin180@gmail.com>
2021-06-04 09:49:58 -0500

    Añado descripción de lo que hace el código

--- BUBBLE SORT/bubleSort.rb
+++ BUBBLE SORT/bubleSort.rb
@@ -1,5 +1,8 @@
 def bubble_sort(array)
     aux = array[0]
+    #Utilizamos 2 bucles para recorrer por todo el arreglo, y comparar si la posición actual es mayor a la siguiente
+    #si esa condición es verdadera, entonces podemos intercambiar posiciones haciendo uso de una varibale auxiliar
+    #para no alterar el arreglo
     for i in 0..array.length - 2 do
         for j in 0..array.length - 2 do
             if array[j] > array[j + 1]
