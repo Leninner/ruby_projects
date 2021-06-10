8bc25964c8d15cb445431f85b06290163cb841b7
Leninner <Mazabandalenin180@gmail.com>
2021-06-10 14:59:20 -0500

    Añado el código para transformar números integer a números romanos

---
+++ Variados/romanos_recursivos.rb
@@ -0,0 +1,26 @@
+roman_mapping = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+}
+
+def integer_to_roman(roman_mapping, number, result = "")
+    return result if number == 0
+    roman_mapping.keys.each do |divisor|
+        cociente, modulus = number.divmod(divisor)
+        result << roman_mapping[divisor] * cociente
+        return integer_to_roman(roman_mapping, modulus, result) if cociente > 0
+    end
+end
+
+integer_to_roman(roman_mapping, 85)
